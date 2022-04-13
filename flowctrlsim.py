9620601a5a753f11796f9f9e69e2a4ff11ac5c59
Joao <joao.vlnunes@outlook.com>
2022-04-13 20:46:56 -0300

    improved simulation of lost packets

--- flowctrlsim.py
+++ flowctrlsim.py
@@ -15,10 +15,16 @@ def flow_control_simulation(protocol: ProtocolsEnum, sequence_of_bits: int, numb
                             lost_packets: List[int]):
     if protocol == ProtocolsEnum.STOP_AND_WAIT_ARQ:
         sequence_of_bits = 1
+
     receiver_queue = Queue(maxsize=1)
+    receiver_channel_queue = Queue(maxsize=1)
+
     sender_queue = Queue(maxsize=1)
-    channel_queue = Queue(maxsize=1)
+    sender_channel_queue = Queue(maxsize=1)
+
     quit_signal = Queue(maxsize=1)
+
+    packet_counter_updater_queue = Queue(maxsize=1)
 
     packages = Queue(maxsize=number_of_frames)
     for i in range(1, number_of_frames):
@@ -34,7 +40,7 @@ def flow_control_simulation(protocol: ProtocolsEnum, sequence_of_bits: int, numb
             while not ack:
                 print(
                     f"Envio do frame {payload} com seq {sequence_number}: A ->> B: ({payload}) Frame {sequence_number}")
-                channel_queue.put(packet)
+                sender_channel_queue.put(packet)
                 try:
                     ack = sender_queue.get(block=True, timeout=2)
                     sequence_number = int(not sequence_number)
@@ -54,34 +60,42 @@ def flow_control_simulation(protocol: ProtocolsEnum, sequence_of_bits: int, numb
             if sequence_number == expected_sequence_number:
                 expected_sequence_number = int(not expected_sequence_number)
             print(f"ACK do frame {sequence_number}: B -->> A: ACK {sequence_number + 1}")
-            channel_queue.put(ack)
+            receiver_channel_queue.put(ack)
 
-    def listen_and_forward():
-        packet_counter = 1
-        packet_lost = False
+    def sender_listen_and_forward():
         while True:
-            packet = channel_queue.get(block=True)
-            # Packet is always sent
-            receiver_queue.put(packet)
-            ack = channel_queue.get(block=True)
-            # Packet loss will be simulated on ACKs
+            packet = sender_channel_queue.get(block=True)
+            packet_counter_updater_queue.put(1, block=True)
             if packet_counter in lost_packets:
-                if not packet_lost:
-                    packet_lost = True
-                    continue
-            sender_queue.put(ack)
+                continue
+            receiver_queue.put(packet)
+
+    def receiver_listen_and_forward():
+        while True:
+            packet = receiver_channel_queue.get(block=True)
+            packet_counter_updater_queue.put(1, block=True)
+            if packet_counter in lost_packets:
+                continue
+            sender_queue.put(packet)
+
+    # We're using a packet_counter_updater to avoid any concurrency problems that might arise
+    def packet_counter_updater():
+        global packet_counter
+        while True:
+            packet_counter_updater_queue.get(block=True)
             packet_counter += 1
-            packet_lost = False
 
     protocol_functions_dictionary = {
         ProtocolsEnum.STOP_AND_WAIT_ARQ: {"sender_function": saw_sender, "receiver_function": saw_receiver}
     }
 
-    protocol_funcions = protocol_functions_dictionary.get(protocol)
+    protocol_functions = protocol_functions_dictionary.get(protocol)
 
-    threading.Thread(target=protocol_funcions["sender_function"]).start()
-    threading.Thread(target=protocol_funcions["receiver_function"]).start()
-    threading.Thread(target=listen_and_forward).start()
+    threading.Thread(target=protocol_functions["sender_function"]).start()
+    threading.Thread(target=protocol_functions["receiver_function"]).start()
+    threading.Thread(target=sender_listen_and_forward).start()
+    threading.Thread(target=receiver_listen_and_forward).start()
+    threading.Thread(target=packet_counter_updater).start()
     quit_signal.get(block=True)
     print("quit received, ending")
 
@@ -95,6 +109,10 @@ arg_protocol = ProtocolsEnum(arguments[1])
 arg_sequence_of_bits = int(arguments[2])
 arg_number_of_frames = int(arguments[3])
 arg_lost_packets = [int(i) for i in arguments[4].split(',')]
+
+# We'll be keeping count of a global packet counter so that we can simulate loss of both frames and ACKs
+# Packet counter will increase on failure as well, so packet_counter will not be synchronized to the frame number
+packet_counter = 1
 
 flow_control_simulation(protocol=arg_protocol, sequence_of_bits=arg_sequence_of_bits,
                         number_of_frames=arg_number_of_frames,
